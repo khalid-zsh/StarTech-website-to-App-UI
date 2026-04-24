c99115e0ee8d285adb8dcff95a8b8e4f3befd400
khalid-zsh <eyeshotkhalid@gmail.com>
2026-04-24 16:24:43 +0600

    10th Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:startech_website_ui/views/HomeView/home_view.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +12,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home:  HomeView(),
+      home:  HomeView (),
     );
   }
 }
